e45d4be487c85e0e29b18ad49b5a9763e751509c
ArefinShaon <arefinshaon99@gmail.com>
2023-06-08 03:48:49 +0600

    Instructor page added

--- src/Pages/InstructorTable/InstructorTable.jsx
+++ src/Pages/InstructorTable/InstructorTable.jsx
@@ -1,9 +1,29 @@
-
+import { useQuery } from "@tanstack/react-query";
 
 const InstructorTable = () => {
+  const { isLoading, isError, data, error } = useQuery([""], async () => {
+    const response = await fetch("http://localhost:5000/class");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  });
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-64">
+        <span className="loading loading-spinner text-warning"></span>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <div className="instructors-page">
-      <h1 className="text-3xl font-bold mb-4">Instructors</h1>
+    <div className="instructors-page mt-24">
+      <h1 className="text-3xl font-bold text-center m-4">THE INSTRUCTORS</h1>
       <table className="w-full border-collapse">
         <thead>
           <tr>
@@ -13,19 +33,23 @@ const InstructorTable = () => {
           </tr>
         </thead>
         <tbody>
-          {/* {instructors.map((instructor) => (
-            <tr key={instructor.id}>
+          {data.map((instructor) => (
+            <tr key={instructor._id}>
               <td className="px-4 py-2">
                 <img
-                  src={instructor.image}
+                  src={instructor.instructor_image}
                   alt={instructor.name}
-                  className="w-12 h-12 rounded-full"
+                  className="w-12 h-12 rounded-full block mx-auto"
                 />
               </td>
-              <td className="px-4 py-2">{instructor.name}</td>
-              <td className="px-4 py-2">{instructor.email}</td>
+              <td className="px-4 py-2 text-center">
+                {instructor.instructor_name}
+              </td>
+              <td className="px-4 py-2 text-center">
+                {instructor.instructor_email}
+              </td>
             </tr>
-          ))} */}
+          ))}
         </tbody>
       </table>
     </div>
